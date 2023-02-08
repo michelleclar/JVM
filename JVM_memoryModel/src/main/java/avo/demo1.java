60e29ec3f73336c5e7a32eacf6cc8d2eb23f888d
carl <carlmichelle493@gmail.com>
2023-02-08 10:38:53 +0800

    类加载

--- JVM_memoryModel/src/main/java/avo/demo1.java
+++ JVM_memoryModel/src/main/java/avo/demo1.java
@@ -6,4 +6,32 @@ package avo;
  * @author: Mr.Carl
  **/
 public class demo1 {
+    static int i = 0;
+
+    static Object obj = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            synchronized (obj) {
+                for (int j = 0; j < 50000; j++) {
+                    i++;
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (obj) {
+                for (int j = 0; j < 50000; j++) {
+                    i--;
+                }
+            }
+        });
+        //启动线程
+        t1.start();
+        t2.start();
+        //等待线程结束
+        t1.join();
+        t2.join();
+        System.out.println(i);
+    }
 }
