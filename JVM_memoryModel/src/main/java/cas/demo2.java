0045524a0fe9eee16eff83fd73a8003c41fdabc6
carl <carlmichelle493@gmail.com>
2023-02-08 15:06:58 +0800

    JMM

--- JVM_memoryModel/src/main/java/cas/demo2.java
+++ JVM_memoryModel/src/main/java/cas/demo2.java
@@ -1,28 +1,25 @@
-package model;
+package cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @program: JVM
- * @description: 原子性
+ * @description: AtomicInteger
  * @author: Mr.Carl
  **/
-public class demo1 {
+public class demo2 {
 
-    static int i = 0;
-    static Object obj = new Object();
-
+    private static AtomicInteger i = new AtomicInteger(0);
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
             for (int j = 0; j < 5000; j++) {
-                synchronized (obj) {
-                    i++;
-                }
+                i.getAndIncrement(); // 获取并且自增 i++
+// i.incrementAndGet(); // 自增并且获取 ++i
             }
         });
         Thread t2 = new Thread(() -> {
             for (int j = 0; j < 5000; j++) {
-                synchronized (obj) {
-                    i--;
-                }
+                i.getAndDecrement(); // 获取并且自减 i--
             }
         });
         t1.start();
--- JVM_memoryModel/src/main/java/jmh/model/demo2.java
+++ JVM_memoryModel/src/main/java/jmh/model/demo2.java
@@ -1,4 +1,4 @@
-package model;
+package jmh.model;
 
 /**
  * @program: JVM
